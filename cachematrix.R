1b09a8c64c7ccf1ea22f36f5e749464f1c2269f8
largilla <largilla@yahoo.com>
2015-07-24 23:45:26 -0700

    commit cachematrix.R to ProgrammingAssignment2 repository

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions produce a mechanism for solving for the inverse of an
+## invertible square matrix, as well as for caching the inverse so that the 
+## calculation need only be performed once.
 
-## Write a short comment describing this function
+## makeCacheMatrix() provides definitions for anonymous function calls 
+## for four operations.: set, get, setinverse, and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() is designed to be an implementation of the anonymous
+## function referenced in makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
